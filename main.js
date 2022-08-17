bfb90503ca0b1ceb640d8d3938bcb1b77295eec1
Nour Wael <92162362+NourWaell@users.noreply.github.com>
2022-08-17 13:45:32 +0200

    Made the countdown timer dynamic

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-let span = document.querySelector(".stotop");
+// Scroll To Top Button
+let scrollToTop = document.querySelector(".stotop");
 window.onscroll = function () {
-  this.scrollY ? span.classList.add("show") : span.classList.remove("show");
+  this.scrollY
+    ? scrollToTop.classList.add("show")
+    : scrollToTop.classList.remove("show");
 };
-span.onclick = function () {
+scrollToTop.onclick = function () {
   window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
 };
 
+// Mega Menu
 let toggler = document.querySelector(".toggle");
 let mega = document.querySelector(".mega-menu");
 let close = document.querySelector(".close-mega");
@@ -33,3 +37,28 @@ for (var i = 0; i < lis.length; i++) {
     }
   });
 }
+
+// CountDown Timer
+let countDownDate = new Date("Nov 20, 2022 23:59:59").getTime();
+let counter = setInterval(() => {
+  let dateNow = new Date().getTime();
+  let dateDifference = countDownDate - dateNow;
+
+  let days = Math.floor(dateDifference / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (dateDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutes = Math.floor((dateDifference % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDifference % (1000 * 60)) / 1000);
+
+  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes").innerHTML =
+    minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds").innerHTML =
+    seconds < 10 ? `0${seconds}` : seconds;
+
+  if (dateDifference < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
